bfedac0a78e4799d6e9f2fa8b710f50eedcbce23
mdeora25 <mahip.deora@gmail.com>
2021-01-09 13:18:42 -0600

    Updates

---
+++ precache-manifest.78e5ec541a5e70c470c16a66bf9776dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37afdb37feb3b192dcdf83263ada42e",
+    "url": "/website_mahip/index.html"
+  },
+  {
+    "revision": "eefe7a7ca230e53d5f34",
+    "url": "/website_mahip/static/css/main.cbd74dd3.chunk.css"
+  },
+  {
+    "revision": "d58e29d5b21d63997aec",
+    "url": "/website_mahip/static/js/2.60565dbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website_mahip/static/js/2.60565dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefe7a7ca230e53d5f34",
+    "url": "/website_mahip/static/js/main.98369420.chunk.js"
+  },
+  {
+    "revision": "54d532cef9e04fc04dcb",
+    "url": "/website_mahip/static/js/runtime-main.74a10d65.js"
+  },
+  {
+    "revision": "9eeeff18553a6b631aec9bf1a45b477e",
+    "url": "/website_mahip/static/media/beach.9eeeff18.JPG"
+  },
+  {
+    "revision": "49fe66d2ed95f298ef05605b88a9dd54",
+    "url": "/website_mahip/static/media/headshot1.49fe66d2.png"
+  },
+  {
+    "revision": "4539562e5d4fbd6b5828d699e1cb46be",
+    "url": "/website_mahip/static/media/resume.4539562e.pdf"
+  }
+]);
